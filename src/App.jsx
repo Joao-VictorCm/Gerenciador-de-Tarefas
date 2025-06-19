0bdf562fa9ffea172dc7f5ba69463abdf27790c9
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-06-19 20:36:13 -0300

    Iniciando a lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,24 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = task.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTask(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Task
         </h1>
-        <Tasks tasks={task} />
+        <Tasks tasks={task} onTaskClick={onTaskClick} />
         <AddTasks />
       </div>
     </div>
